b980ab300952c1aadb893f87deb7358648d1d91e
Andrius Žimkus <and.zimkus@gmail.com>
2025-12-01 17:16:04 +0200

    2025-12-01_17:16:04

---
+++ 2025/Day1/SecretEntrance.py
@@ -0,0 +1,67 @@
+def main():
+ 
+    fileName = "test.txt"
+    path = "../../../Advent Of Code Cases/2025/Day1/" + fileName
+
+    rotations = []
+    
+    with open(path, 'r') as file:
+        for line in file:
+            rotations.append(line.strip())
+
+    clickCount = 0
+    currentPos = 50
+    countOfClicksSimple = 0
+    countOfClicksComplex = 0
+
+    for rotation in rotations:
+        currentPos, clickCount = applyRotation(currentPos, rotation, "simple")
+        countOfClicksSimple += clickCount
+
+    currentPos = 50
+
+    for rotation in rotations:
+        currentPos, clickCount = applyRotation(currentPos, rotation, "0x434C49434B")
+        countOfClicksComplex += clickCount
+
+
+    print("Simple: ", countOfClicksSimple)
+    print("0x434C49434B: ", countOfClicksComplex)
+        
+def applyRotation(currentPos, rotation, method):
+
+    finalPos = currentPos
+
+    direction = rotation[0]
+    clickCount = 0
+    
+    amount = int(rotation[1:])
+    complexClicks = amount // 100
+
+    amount = amount%100
+    
+    if direction == "L":
+        finalPos = currentPos - amount
+        if finalPos < 0:
+            if method == "0x434C49434B":
+                clickCount += 1
+            finalPos *= -1
+            finalPos = 100 - finalPos
+    else:
+        finalPos = currentPos + amount
+        if finalPos >= 100:
+            if method == "0x434C49434B":
+                clickCount += 1
+            finalPos %= 100
+
+    if finalPos == 0:
+        clickCount += 1
+
+    if method == "0x434C49434B":
+        clickCount += complexClicks
+    if method == "0x434C49434B":
+        print(rotation, clickCount)
+    return finalPos,clickCount
+        
+if __name__ == "__main__":
+    main()
