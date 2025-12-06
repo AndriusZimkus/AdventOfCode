b6f136b7df8b67c2777dc509b93490017669cce5
Andrius Žimkus <and.zimkus@gmail.com>
2025-12-06 22:12:51 +0200

    2025 Day 6 - Part 2

--- 2025/Day6/TrashCompactor.py
+++ 2025/Day6/TrashCompactor.py
@@ -1,10 +1,9 @@
 def main():
  
-    fileName = "test.txt"
+    fileName = "input.txt"
     path = "../../../Advent Of Code Cases/2025/Day6/" + fileName
 
     with open(path, 'r') as file:
-
         sets = {}
         for line in file:
             i = 0
@@ -14,12 +13,61 @@ def main():
                     sets[i].append(row[i])
                 else:
                     sets[i] = [row[i]]
+
+    total = getSetsTotal(sets)
+    print ("First total: ", total)
+
+    listsForSets = []
+    with open(path, 'r') as file:
+        sets = {}
+        for line in file:
+            listsForSets.append(list(line))
+
+    sets = {}
+
+
+    setKey = 0
+    length = len(listsForSets[0])-1
+
+    column = length - 1
+    symbolFound = False
+
+    while column >= 0:
+        currentNumber = 0
+        for row in range(len(listsForSets)):
+            
+            currentList = listsForSets[row]
+            
+            currentSymbol = currentList[column].strip()
+
+            if currentSymbol == "*" or currentSymbol == "+":
+                symbolFound = True
+  
+            else:
+                if currentSymbol:
+                    currentNumber = currentNumber*10 + int(currentSymbol)
+                
+        if currentNumber > 0:
+            if setKey in sets:
+                sets[setKey].append(currentNumber)
+            else:   
+                sets[setKey] = [currentNumber]
+        column-=1
+
+        if symbolFound:
+            symbolFound = False
+            sets[setKey].append(currentSymbol)
+            setKey += 1
+
+    total = getSetsTotal(sets)
+    print ("Second total: ", total)
+
+
+def getSetsTotal(sets):
     total = 0
     for key in sets.keys():
         currentSet = sets[key]
-        #print(currentSet)
         action = currentSet[len(currentSet)-1]
-        #print(action)
         if action == '*':
             current = 1
         else:
@@ -30,15 +78,9 @@ def main():
             else:
                 current+=int(currentSet[i])
 
-        #print (current)
         total += current
 
-    print ("Total: ", total)
-
-    with open(path, 'r') as file:
-        sets = {}
-        for line in file:
-            print(list(line))
+    return total
 
 if __name__ == "__main__":
     main()
